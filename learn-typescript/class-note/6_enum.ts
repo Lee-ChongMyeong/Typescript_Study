15dbac27b324e890e8d07d647802fb41eba33216
cleverlee <dlchdaud1327@gmail.com>
2021-06-30 01:32:59 +0900

    스터디 업로드

--- learn-typescript/class-note/6_enum.ts
+++ learn-typescript/class-note/6_enum.ts
@@ -1,8 +1,38 @@
 enum Shoes {
-    Nike,
-    Adidas,
-    Sth,
+    Nike = '나이키',
+    Adidas = '아디다스'
+}
+var myShoes = Shoes.Nike;
+console.log(myShoes);   // '나이키'
+
+// 예제
+enum Answer {
+    Yes = 'Y',
+    No = 'N'
 }
 
-var myShoes = Shoes.Nike;
-console.log(myShoes);   // 0 
+// 예제
+// function askQuestion(answer: string){
+//     if(answer ==='yes'){
+//         console.log('정답입니다');
+//     }
+//     if (answer === 'no'){
+//         console.log('오답입니다');
+//     }
+// }
+
+function askQuestion(answer: string){
+    if(answer ===Answer.Yes){
+        console.log('정답입니다');
+    }
+    if (answer === Answer.No){
+        console.log('오답입니다');
+    }
+}
+
+// askQuestion('예스');
+// askQuestion('y');
+// askQuestion('Yes');
+
+askQuestion(Answer.Yes);
+askQuestion('Yes'); // error
